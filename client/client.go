764233d5f5f6eabcce777d7d2c7949ff60a4ada5
Emanuele <egentili2000@gmail.com>
2024-05-02 17:15:10 +0200

    Now client launch parallel request

--- client/client.go
+++ client/client.go
@@ -31,9 +31,6 @@ func sendRequest(params []int, service string) {
 	var ret Return
 	loadBalancerAddress := os.Getenv("LOAD_BALANCER")
 	loadBalancer, err := rpc.Dial("tcp", loadBalancerAddress)
-	defer func(loadBalancer *rpc.Client) {
-		_ = loadBalancer.Close()
-	}(loadBalancer)
 	for _, param := range params {
 		args.Service = service
 		args.Input = param
@@ -41,11 +38,17 @@ func sendRequest(params []int, service string) {
 			fmt.Printf("An error occured : %s \n", err)
 		}
 		done := loadBalancer.Go("LoadBalancer.ServeRequest", args, &ret, nil)
-		done = <-done.Done
-		if done.Error != nil {
-			fmt.Printf("An error occured : %s \n", done.Error)
-		}
-		fmt.Printf("The result of %s(%d) is %d\n", args.Service, args.Input, ret)
+		go waitResult(done, args, &ret)
+	}
+}
+
+func waitResult(done *rpc.Call, args Args, p *Return) {
+	done = <-done.Done
+	if done.Error != nil {
+		fmt.Printf("An error occured %s \n", done.Error)
+		os.Exit(1)
+	} else {
+		fmt.Printf("The result of %s(%d) is %d\n", args.Service, args.Input, *p)
 	}
 }
 
